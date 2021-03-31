55cf9d6d3e4c6e788f6e52094c4736905c1e1ed8
Markiesch <marksmeel@gmail.com>
2021-03-31 15:10:40 +0200

    Backup release 31-3-2021

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-    echo "hello world";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/stylesheet.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <section class="intro">
+            <div>
+                <h1>ICT-ACADEMIE</h1>
+                <p>De toekomst in jouw handen</p>
+                <a href="">Studiekeuzetest</a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
+

e30b42e47767fdca46978ed51ca06babf476c2dc
Markiesch <marksmeel@gmail.com>
2021-04-01 11:29:36 +0200

    added include

--- index.php
+++ index.php
@@ -11,13 +11,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/stylesheet.css">
+    <link rel="stylesheet" href="styles/includes.css">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <script src="scripts/script.js" defer></script>
     <title>ICT-Academie - Koning Willem I College</title>
 </head>
 <body>
+    <?php 
+        include "includes/header.php"
+    ?>
     <main>
         <section class="intro">
-            <!-- <div class="intro--background"></div> -->
+            <div class="intro--background"></div>
             <div>
                 <h1>ICT-ACADEMIE</h1>
                 <h3>De toekomst in jouw handen</h3>

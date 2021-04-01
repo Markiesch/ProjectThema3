b9d8718b95abd0850ef360001fc86e36c3443214
Markiesch <marksmeel@gmail.com>
2021-04-01 11:02:50 +0200

    Responsive text + first section homepage

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<!-- 
+    Author: Mark Schuurmans
+    Date: 31-3-2021
+    File: index.php
+ -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,16 +11,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/stylesheet.css">
-    <title>Document</title>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <title>ICT-Academie - Koning Willem I College</title>
 </head>
 <body>
     <main>
         <section class="intro">
+            <!-- <div class="intro--background"></div> -->
             <div>
                 <h1>ICT-ACADEMIE</h1>
-                <p>De toekomst in jouw handen</p>
+                <h3>De toekomst in jouw handen</h3>
                 <a href="">Studiekeuzetest</a>
             </div>
+        </section>
+        <div class="streep"></div>
+        <section>
+            <div></div>
+            <div></div>
         </section>
     </main>
 </body>

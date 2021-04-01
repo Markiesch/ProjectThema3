b137b98256daacdf6711f88e48cf4ee131c0fe5d
Administrator <pepijn.emmers@edu-kw1c.nl>
2021-04-01 11:15:12 +0200

    includes invoegen 1-4-2021

--- pages/formulier.php
+++ pages/formulier.php
@@ -1,1 +1,40 @@
-<?php
+<!--
+    Author: Pepijn Emmers
+    Date: 1-4-2021
+    File: formulier.php
+ -->
+
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../styles/stylesheet.css">
+        <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+        <title>ICT-Academie - Koning Willem I College</title>
+    </head>
+    <body>
+        <?php
+            include "../includes/header.php";
+        ?>
+        <main>
+            <section class="intro">
+                <div>
+                    <h1>ICT-ACADEMIE</h1>
+                    <h3>De toekomst in jouw handen</h3>
+                    <a href="">Studiekeuzetest</a>
+                </div>
+            </section>
+            <div class="streep"></div>
+            <section>
+                <div></div>
+                <div></div>
+            </section>
+        </main>
+        <?php
+            include "../includes/footer.php";
+        ?>
+    </body>
+</html>
+

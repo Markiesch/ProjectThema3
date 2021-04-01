f26b04ef291a3dce4d1e6520d05ca515f032dcdb
Administrator <pepijn.emmers@edu-kw1c.nl>
2021-04-01 11:24:00 +0200

    1-4-2021

--- pages/resultaat.php
+++ pages/resultaat.php
@@ -11,30 +11,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/stylesheet.css">
+    <link rel="stylesheet" href="../styles/includes.css">
+    <script src="../scripts/script.js" defer></script>
     <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
     <title>ICT-Academie - Koning Willem I College</title>
 </head>
-<body>
-<?php
-    include "../includes/header.php";
-?>
-<main>
-    <section class="intro">
-        <div>
-            <h1>ICT-ACADEMIE</h1>
-            <h3>De toekomst in jouw handen</h3>
-            <a href="">Studiekeuzetest</a>
-        </div>
-    </section>
-    <div class="streep"></div>
-    <section>
-        <div></div>
-        <div></div>
-    </section>
-</main>
-<?php
-    include "../includes/footer.php";
-?>
-</body>
+    <body>
+        <?php
+            include "../includes/header.php";
+        ?>
+        <main>
+            <section class="intro">
+                <div>
+                    <h1>ICT-ACADEMIE</h1>
+                    <h3>De toekomst in jouw handen</h3>
+                    <a href="">Studiekeuzetest</a>
+                </div>
+            </section>
+            <div class="streep"></div>
+            <section>
+                <div></div>
+                <div></div>
+            </section>
+        </main>
+        <?php
+            include "../includes/footer.php";
+        ?>
+    </body>
 </html>
 

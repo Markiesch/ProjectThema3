5af534fef696ef40a04d64b135e37e11a5eef616
Markiesch <marksmeel@gmail.com>
2021-04-01 12:05:31 +0200

    Added link

--- index.php
+++ index.php
@@ -3,38 +3,36 @@
     Date: 31-3-2021
     File: index.php
  -->
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/stylesheet.css">
-    <link rel="stylesheet" href="styles/includes.css">
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-    <script src="scripts/script.js" defer></script>
-    <title>ICT-Academie - Koning Willem I College</title>
-</head>
-<body>
-    <?php 
-        include "includes/header.php"
-    ?>
-    <main>
-        <section class="intro">
-            <div class="intro--background"></div>
-            <div>
-                <h1>ICT-ACADEMIE</h1>
-                <h3>De toekomst in jouw handen</h3>
-                <a href="">Studiekeuzetest</a>
-            </div>
-        </section>
-        <div class="streep"></div>
-        <section>
-            <div></div>
-            <div></div>
-        </section>
-    </main>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles/stylesheet.css">
+        <link rel="stylesheet" href="styles/includes.css">
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <script src="scripts/script.js" defer></script>
+        <title>ICT-Academie - Koning Willem I College</title>
+    </head>
+    <body>
+        <?php 
+            include "includes/header.php"
+        ?>
+        <main>
+            <section class="intro">
+                <div class="intro--background"></div>
+                <div>
+                    <h1>ICT-ACADEMIE</h1>
+                    <h3>De toekomst in jouw handen</h3>
+                    <a href="pages/formulier.php">Studiekeuzetest</a>
+                </div>
+            </section>
+            <div class="streep"></div>
+            <section>
+                <div></div>
+                <div></div>
+            </section>
+        </main>
+    </body>
 </html>
-

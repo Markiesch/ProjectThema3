4682e66092baf431daa563da26ca2314780ddf87
Administrator <pepijn.emmers@edu-kw1c.nl>
2021-04-01 11:11:38 +0200

    include header.php 1-4-2021

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,186 @@
-<?php
+<header>
+    <img src="/ProjectThema3/images/kw1c-logo.svg" alt="logo" class="logo">
+    <nav id="centerNav">
+        <ul>
+            <li>
+                <a href="/opleidingen">Opleidingen</a>
+            </li>
+            <li id="studietest">
+                <a href="/ProjectThema3/pages/formulier.php">Studiekeuze test</a>
+                <span class="newMenuItem">New</span>
+            </li>
+            <li>
+                <a href="/volwassenen">Volwassenen</a>
+            </li>
+            <li>
+                <a href="/studenteninfo">Studenteninfo</a>
+            </li>
+        </ul>
+    </nav>
+    <div id="menuRight">
+        <div id="headerIcons">
+            <a href="/">
+                <img src="../images/foreign-visitors.svg" alt="foreign-visitors" id="foreign-visitors" title="infomation for foreign-visitors">
+            </a>
+            <a href="/">
+                <svg id="lock" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve"><g><path xmlns="http://www.w3.org/2000/svg" d="m336 192h-16v-64c0-70.59375-57.40625-128-128-128s-128 57.40625-128 128v64h-16c-26.453125 0-48 21.523438-48 48v224c0 26.476562 21.546875 48 48 48h288c26.453125 0 48-21.523438 48-48v-224c0-26.476562-21.546875-48-48-48zm-229.332031-64c0-47.0625 38.269531-85.332031 85.332031-85.332031s85.332031 38.269531 85.332031 85.332031v64h-170.664062zm0 0" data-original="#000000"/></g></svg>
+            </a>
+            <a href="/">
+                <svg id="search" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path xmlns="http://www.w3.org/2000/svg" d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" data-original="#000000"/><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g><g xmlns="http://www.w3.org/2000/svg">
+                        </g></g></svg>
+            </a>
+        </div>
+        <div id="hamburger" onclick="toggleMenu()">
+            <div id="lines">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+            <span id="mobileBurger">Menu</span>
+        </div>
+        <div id="slideMenuTriangle"></div>
+    </div>
+    <div id="slideMenu" class="menuClosed">
+        <!-- icons mobile -->
+        <a href="/" id="mobileLock" title="Het Portaal">
+            <img src="/ProjectThema3/images/lock.svg" alt="">
+            <span>Portaal</span>
+        </a>
+        <a href="/" id="mobileSearch" title="Zoeken op deze site">
+            <img src="/ProjectThema3/images/search.svg" alt="">
+            <span>Zoeken</span>
+        </a>
+
+        <!-- menu 1 mobile menu -->
+        <ul id="menu1">
+            <li>
+                <a href="/opleidingen" title="Aanbod van opleidingen" style="background-image: none">Opleidingen</a>
+            </li>
+            <li id="studietest">
+                <a href="#" title="Doe nu de studiekeuze test!" style="background-image: none">Studiekeuze test</a>
+                <span class="newMenuItem">New</span>
+            </li>
+            <li>
+                <a href="/volwassenen-werkgevers/trainingen-opleidingen-en-cursussen" title="Volwassenen &amp; werkgevers" style="background-image: none">Volwassenen</a>
+            </li>
+            <li>
+                <a href="/studenteninfo" title="Informatie voor studenten" style="background-image: none">Studenteninfo</a>
+            </li>
+        </ul>
+        <!-- menu 2 slide menu -->
+        <ul id="menu2">
+            <li class="withSubmenu" id="sub1">
+                <span class="subtitle" title="Over ons">Over ons</span>
+                <ul class="submenu">
+                    <li>
+                        <a href="/rookvrij-schoolterrein">Rookvrij schoolterrein</a>
+                    </li>
+                    <li>
+                        <a href="/community-college">Community college</a>
+                    </li>
+                    <li>
+                        <a href="/wie-zijn-wij">Wie zijn wij?</a>
+                    </li>
+                    <li>
+                        <a href="/afdelingen">Afdelingen</a>
+                    </li>
+                    <li>
+                        <a href="/jaarverslagen">Jaarverslagen</a>
+                    </li>
+                    <li>
+                        <a href="/jaarbeeld">Jaar in Beeld</a>
+                    </li>
+                    <li>
+                        <a href="/organogram">Organogram</a>
+                    </li>
+                    <li>
+                        <a href="/missie-visie-kern-en-merkwaarden">Missie visie kern- en merkwaarden</a>
+                    </li>
+                    <li>
+                        <a href="/foreign-visitors">Foreign visitors</a>
+                    </li>
+                    <li>
+                        <a href="/unesco-school">UNESCO-school</a>
+                    </li>
+                    <li>
+                        <a href="/anbi-status">ANBI-status</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="withSubmenu" id="sub2">
+                <span class="subtitle" title="Nieuws">Nieuws</span>
+                <ul class="submenu">
+                    <li>
+                        <a href="/agenda">Agenda</a>
+                    </li>
+                    <li>
+                        <a href="/jaarbeeld">Jaar in Beeld</a>
+                    </li>
+                    <li>
+                        <a href="/nieuws">Nieuwsberichten</a>
+                    </li>
+                    <li>
+                        <a href="/persberichten">Persberichten</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="/activiteiten" title="Activiteiten op het college" style="background-image: none">Activiteiten</a>
+            </li>
+            <li>
+                <a href="/werken-bij-het-college" target="_blank" title="Werken bij ons" style="background-image: none">Werken bij ons</a>
+            </li>
+            <li class="withSubmenu" id="sub3">
+                <span class="subtitle" title="Contact">Contact</span>
+                <ul class="submenu">
+                    <li>
+                        <a href="/locaties">Locaties</a>
+                    </li>
+                    <li>
+                        <a href="/routebeschrijving">Routebeschrijving</a>
+                    </li>
+                    <li>
+                        <a href="/contact/vraag">Stuur ons een e-mail</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="withSubmenu" id="sub4">
+                <span class="subtitle" title="Meer">Meer</span>
+                <ul class="submenu">
+                    <li>
+                        <a href="/school-voor-de-toekomst">School voor de Toekomst</a>
+                    </li>
+                    <li>
+                        <a href="/aanbestedingen">Aanbestedingen</a>
+                    </li>
+                    <li>
+                        <a href="/studenteninfo-van-a-tot-z">Studenteninfo-van-a-tot-z</a>
+                    </li>
+                    <li>
+                        <a href="/het-innovatielab">Het Innovatielab</a>
+                    </li>
+                    <li>
+                        <a href="/ondernemerschap">Ondernemerschap</a>
+                    </li>
+                    <li>
+                        <a href="/projecten">Projecten</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</header>
---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+var hamburger = document.getElementById("hamburger");
+var slideMenu = document.getElementById("slideMenu");
+
+function toggleMenu()
+{
+    //toggle menu
+    if(slideMenu.className == "menuClosed")
+    {
+        slideMenu.className = "menuOpen";
+        hamburger.className = "closeMenu";
+        document.getElementById("slideMenuTriangle").style.opacity = 1;
+    }
+    else
+    {
+        slideMenu.className = "menuClosed";
+        hamburger.classList.remove("closeMenu");
+        document.getElementById("slideMenuTriangle").style.opacity = 0;
+    }
+}
+
+//submenu's openen
+var sub1 = document.getElementById("sub1");
+var sub2 = document.getElementById("sub2");
+var sub3 = document.getElementById("sub3");
+var sub4 = document.getElementById("sub4");
+
+sub1.addEventListener("click", openSub1);
+sub2.addEventListener("click", openSub2);
+sub3.addEventListener("click", openSub3);
+sub4.addEventListener("click", openSub4);
+
+function openSub1(){
+    if(document.querySelector("#sub1 > .submenu").style.display == "block"){
+        document.querySelector("#sub1 > .submenu").style.display = "none";
+    }else{
+        document.querySelector("#sub1 > .submenu").style.display = "block";
+    }
+}
+function openSub2(){
+    if(document.querySelector("#sub2 > .submenu").style.display == "block"){
+        document.querySelector("#sub2 > .submenu").style.display = "none";
+    }else{
+        document.querySelector("#sub2 > .submenu").style.display = "block";
+    }
+}
+function openSub3(){
+    if(document.querySelector("#sub3 > .submenu").style.display == "block"){
+        document.querySelector("#sub3 > .submenu").style.display = "none";
+    }else{
+        document.querySelector("#sub3 > .submenu").style.display = "block";
+    }
+}
+function openSub4(){
+    if(document.querySelector("#sub4 > .submenu").style.display == "block"){
+        document.querySelector("#sub4 > .submenu").style.display = "none";
+    }else{
+        document.querySelector("#sub4 > .submenu").style.display = "block";
+    }
+}
+

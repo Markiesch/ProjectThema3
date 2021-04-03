57b7e121132938c638e1f0034686708f89c74570
Administrator <pepijn.emmers@edu-kw1c.nl>
2021-04-03 13:33:30 +0200

    responsive fixed

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="styles/stylesheet.css">
-        <link rel="stylesheet" href="styles/includes.css">
         <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
         <script src="scripts/script.js" defer></script>
         <title>ICT-Academie - Koning Willem I College</title>

0c9daf2d8028c5fa76fccf78dfe8d2120c593382
Administrator <pepijn.emmers@edu-kw1c.nl>
2021-04-08 12:11:37 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,19 @@
 <footer>
     <a href="/privacyverklaring" title="Privacyverklaring van het Koning Willem I College" id="privacyverklaring">Privacyverklaring</a>
     <div id="socialslogo">
-        <a href="facebook.com">
+        <a href="https://www.facebook.com/KoningWillemICollege" target="_blank">
             <img src="/ProjectThema3/images/facebook.svg" alt="facebook" id="facebook" title="facebook" class="logosize">
         </a>
-        <a href="twitter.com">
+        <a href="https://twitter.com/KW1C" target="_blank">
             <img src="/ProjectThema3/images/twitter.svg" alt="twitter" id="twitter" title="twitter" class="logosize">
         </a>
-        <a href="instagram.com">
+        <a href="http://instagram.com/kw1c" target="_blank">
             <img src="/ProjectThema3/images/instagram-logo.svg" alt="instagram" id="instagram" title="instagram" class="logosize">
         </a>
-        <a href="linkedin.com">
+        <a href="https://www.linkedin.com/company/koning-willem-i-college" target="_blank">
             <img src="/ProjectThema3/images/linkedin.svg" alt="linkedin" id="linkedin" title="linkedin" class="logosize">
         </a>
-        <a href="youtube.com">
+        <a href="http://www.youtube.com/KoningWillemICollege" target="_blank">
             <img src="/ProjectThema3/images/youtube.svg" alt="youtube" id="youtube" title="youtube" class="logosize">
         </a>
     </div>

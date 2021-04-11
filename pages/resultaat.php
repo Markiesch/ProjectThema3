99b86fb412f0a94bede2fb68cc6f512ae9a4c8b3
Tobi de Fijter <tobi.defijter@edu-kw1c.nl>
2021-04-11 15:08:14 +0200

    resultaten page back up\

--- pages/resultaat.php
+++ pages/resultaat.php
@@ -11,6 +11,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/stylesheet.css">
+    <link rel="stylesheet" href="../styles/resultaat.css">
+    <link rel="stylesheet" href="../styles/index.css">
     <script src="../scripts/script.js" defer></script>
     <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
     <title>ICT-Academie - Koning Willem I College</title>
@@ -20,7 +22,7 @@
             include "../includes/header.php";
         ?>
         <main>
-            <section class="intro">
+            <section class="intro" class="information--section">
                 <div>
                     <h1>ICT-ACADEMIE</h1>
                     <h2>De toekomst in jouw handen</h2>
@@ -28,9 +30,29 @@
                 </div>
             </section>
             <div class="streep"></div>
-            <section>
-                <div></div>
-                <div></div>
+           
+           
+            <section class="information--section">
+                <div>
+                    <div>
+                        <h2>Resultaat</h2><br>
+                        <p>Bij jou past de opleiding " ….……... " het beste.
+                        Jij hebt de juiste eigenschappen voor deze opleiding
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                    </div>
+                   <div>
+                       
+                   </div>
+                </div>
+                <div>
+                    <div>
+                        <h2>Contact</h2>
+                        <p>
+                            <a href="callto:0736249600">073 624 9 600</a> <br>      
+                            <a href="mailto:info@kw1c.nl">info@kw1c.nl</a>    
+                        </p>    
+                    </div>
+                </div>
             </section>
         </main>
         <?php

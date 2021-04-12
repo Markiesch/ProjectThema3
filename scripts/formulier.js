6a122904a82b43204746015ef7d6ff3039d5f9cd
Markiesch <marksmeel@gmail.com>
2021-04-12 11:09:06 +0200

    improvement proposal Javascript

--- scripts/formulier.js
+++ scripts/formulier.js
@@ -6,10 +6,8 @@
 */
 
 //form beginnen en cookie instellen
-function startTestAddCookie()
-{
-    if(document.getElementById("usernameInput").value != "")
-    {
+function startTestAddCookie() {
+    if (document.getElementById("usernameInput").value) {
         var username = document.getElementById("usernameInput").value;
         document.cookie = "username=" + username + ";expires=Mon, 1 Jan 2024 12:00:00 UTC; path=/";
     }
@@ -17,8 +15,7 @@ function startTestAddCookie()
     document.getElementById("fbuttons").style.cssText = "opacity: 1; pointer-events: all;";
     document.getElementById("fquestions").style.display = "block";
 }
-function startStudieTest()
-{
+function startStudieTest() {
     document.getElementById("formStartscreen").style.display = "none";
     document.getElementById("fbuttons").style.cssText = "opacity: 1; pointer-events: all;";
     document.getElementById("fquestions").style.display = "block";
@@ -38,23 +35,21 @@ if (activeQuestion == 1) {
 }
 
 //vorige vraag button
-function prevQuestion()
-{
+function prevQuestion() {
     //active question verlagen
     activeQuestion -= 1;
     //vraag terug
-    if(activeQuestion == 1) {
+    if (activeQuestion == 1) {
         prevBtn.style.cssText = "opacity: 0; pointer-events: none;";
     }
     changeQuestion(activeQuestion);
 }
 
 //volgende vraag button
-function nextQuestion()
-{
+function nextQuestion() {
     //active question verhogen
     activeQuestion += 1;
-    if(activeQuestion == 2) {
+    if (activeQuestion == 2) {
         prevBtn.style.cssText = "opacity: 1; pointer-events: all;";
     }
 
@@ -64,39 +59,8 @@ function nextQuestion()
 }
 
 //change question
-function changeQuestion(activeQuestion)
-{
-    if (activeQuestion == 1) {
-        questions.style.left = "0%";
-    } else if (activeQuestion == 2) {
-        questions.style.left = "-100%";
-    } else if (activeQuestion == 3) {
-        questions.style.left = "-200%";
-    } else if (activeQuestion == 4) {
-        questions.style.left = "-300%";
-    } else if (activeQuestion == 5) {
-        questions.style.left = "-400%";
-    } else if (activeQuestion == 6) {
-        questions.style.left = "-500%";
-    } else if (activeQuestion == 7) {
-        questions.style.left = "-600%";
-    } else if (activeQuestion == 8) {
-        questions.style.left = "-700%";
-    } else if (activeQuestion == 9) {
-        questions.style.left = "-800%";
-    } else if (activeQuestion == 10) {
-        questions.style.left = "-900%";
-    } else if (activeQuestion == 11) {
-        questions.style.left = "-1000%";
-    } else if (activeQuestion == 12) {
-        questions.style.left = "-1100%";
-    } else if (activeQuestion == 13) {
-        questions.style.left = "-1200%";
-    } else if (activeQuestion == 14) {
-        questions.style.left = "-1300%";
-        nextBtn.style.display = "flex";
-    } else if (activeQuestion == 15) {
-        questions.style.left = "-1400%";
-        nextBtn.style.display = "none";
-    }
+function changeQuestion(activeQuestion) {
+    questions.style.left = (activeQuestion - 1) * -100 + "%";
+    if (activeQuestion == 14) nextBtn.style.display = "flex";
+    if (activeQuestion == 15) nextBtn.style.display = "none";
 }
